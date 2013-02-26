e3bf26f528cf98287d93a1ebb84f6a8687756165
Joseph Dobson <dobson@localhost.localdomain>
2013-02-26 18:08:45 +0000

    worked on input_parser

--- input_parser.cpp
+++ input_parser.cpp
@@ -2,6 +2,8 @@
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_bind.hpp>
 
+#include <boost/spirit/home/support/info.hpp>
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -9,88 +11,72 @@
 #include <algorithm>
 
 void handle_join(const std::vector<std::string>& input) {
-	std::copy(
-		input.cbegin(),
-		input.cend(),
-		std::ostream_iterator<std::string>(std::cout, "\n")
-	);
+	std::cout << "JOIN\n";	
+	std::copy(input.cbegin(), input.cend(),
+		std::ostream_iterator<std::string>(std::cout, "\n"));
 }
-
 void handle_part(const std::string& chan, const std::string msg) {
-	std::cout << chan << " " << msg << '\n';
+	std::cout << "PART " << chan << " " << msg << '\n';
 }
-
 void handle_connect(const std::string& chan) {
-	std::cout << chan << '\n';
+	std::cout << "CONNECT " << chan << '\n';
 }
-
 void handle_nick(const std::string& nick) {
-	std::cout << nick << '\n';
+	std::cout << "NICK " << nick << '\n';
 }
-
 void handle_msg(const std::string& target, const std::string& msg) {
-	std::cout << target << " " << msg << '\n';
+	std::cout << "MSG " << target << "\n" << msg << '\n';
+}
+void handle_text(const std::string& text) {
+	std::cout << text << '\n';
+}
+void handle_exec(const std::string& exec) {
+	std::system(exec.c_str());
+//	std::cout << "CMD: " << exec << '\n';
 }
 
-struct printer
-{
-    typedef boost::spirit::utf8_string string;
-
-    void element(string const& tag, string const& value, int depth) const
-    {
-        for (int i = 0; i < (depth*4); ++i) // indent to depth
-            std::cout << ' ';
-
-        std::cout << "tag: " << tag;
-        if (value != "")
-            std::cout << ", value: " << value;
-        std::cout << std::endl;
-    }
-};
-
-void print_info(boost::spirit::info const& what) {
-    using boost::spirit::basic_info_walker;
-    printer pr;
-    basic_info_walker<printer> walker(pr, what.tag, 0);
-    boost::apply_visitor(walker, what.value);
-}
-
-
-int main() {
-	std::string input="/join hello bob";
-	auto first=input.cbegin(),
-	     last =input.cend();
-
+template<typename Iter>
+void parse(Iter first, Iter last) {
 	namespace qi=boost::spirit::qi;
 	namespace phx=boost::phoenix;
 
-	qi::char_type char_;
-	qi::lit_type  lit;
-	qi::_1_type   _1;
-	qi::_1_type   _2;
-	qi::_val_type _val;
-	qi::string_type string;
+	qi::char_type   char_;
+	qi::lit_type    lit;
+	qi::space_type  space;
+	qi::lexeme_type lexeme;
 
+	qi::_1_type     _1;
+	qi::_2_type     _2;
+
+	//This rule IS space sensitive
 	qi::rule<decltype(first), std::string()> str=+~char_(' ');
 
-	try {
-		qi::parse(
-			first,
-			last,
-			lit('/') >
-			( "join "   >> (str % ' ') [ phx::bind( &::handle_join,    _1 )     ]
-			| "leave"   >> str >> str  [ phx::bind( &::handle_part,    _1, _2 ) ]
-			| "connect" >> str         [ phx::bind( &::handle_connect, _1 )     ]
-			| "nick"    >> str         [ phx::bind( &::handle_nick,    _1 )     ]
-			| "msg"     >> str >> str  [ phx::bind( &::handle_msg,     _1, _2)  ]
+#define CMD_NM(head, tail) lexeme[ lit(head) >> -lit(tail) >> space ]
+	qi::phrase_parse(
+		first,
+		last,
+		( !lit('/') >> str                        [ phx::bind( &::handle_text,    _1 )     ]
+		|'/' 
+		>>  ( CMD_NM('j', "oin" ) >> (+str)       [ phx::bind( &::handle_join,    _1 )     ]
+			| CMD_NM('m', "sg"  ) >> (str >> str) [ phx::bind( &::handle_msg,     _1, _2 ) ]
+			| CMD_NM('l', "eave") >> (str >> str) [ phx::bind( &::handle_part,    _1, _2 ) ]
+			| "connect"           >> str          [ phx::bind( &::handle_connect, _1 )     ]
+			| "nick"              >> str          [ phx::bind( &::handle_nick,    _1 )     ]
+			| "exec"              >> str          [ phx::bind( &::handle_exec,    _1 )     ]
 			)
-		);
+		)
+		, 
+		space
+	);
+#undef CMD_NM
+}
+
+int main() {
+	std::string input="/join hello bob";
+
+	while(std::getline(std::cin, input)) {
+		parse(input.cbegin(), input.cend());
 	}
-	catch(const qi::expectation_failure<decltype(first)> x) {
-		std::cerr << "expected: "; print_info(x.what_);
-		std::cerr << "got: \"" << std::string(x.first, x.last) << '"' << std::endl;
-	}
-	catch(const std::exception& x) {
-		std::cerr << x.what() << std::endl;
-	}
+
+	return EXIT_SUCCESS;
 }
